31e13528e3476a5268978f594cab1f48b1255387
Sean Cross <sean@xobs.io>
2022-02-15 14:49:17 +0800

    wifi: begin wifi client connection
    Signed-off-by: Sean Cross <sean@xobs.io>

--- main/wifi.c
+++ main/wifi.c
@@ -30,9 +30,23 @@ struct device_wifi_config
     uint8_t sta_mask;
 };
 
-#define DEVICE_WIFI_CONFIG_MAGIC 0xaa25d294
+enum wifi_state
+{
+    WifiIdle = 0,
+    WifiApStarted = 1,
+    WifiStaStarted = 2,
+};
 
-static struct device_wifi_config device_wifi_config;
+struct device_wifi_config_handle
+{
+    struct device_wifi_config config;
+    bool dirty;
+    nvs_handle nvs;
+    enum wifi_state state;
+    esp_netif_t *netif;
+};
+
+#define DEVICE_WIFI_CONFIG_MAGIC 0xaa25d294
 
 // case SYSTEM_EVENT_STA_START:
 static void esp_system_event_sta_start(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
@@ -61,7 +75,7 @@ static void esp_system_event_sta_got_ip(void *event_handler_arg, esp_event_base_
 static void esp_system_event_sta_disconnected(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
     wifi_event_sta_disconnected_t *info = event_data;
-    ESP_LOGE(TAG, "Disconnect reason : %d", info->reason);
+    ESP_LOGE(TAG, "Disconnect reason : %d (%s)", info->reason, esp_err_to_name(info->reason));
     // if (info->disconnected.reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
     //     /*Switch to 802.11 bgn mode */
     //     //esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCAL_11B | WIFI_PROTOCAL_11G | WIFI_PROTOCAL_11N);
@@ -87,13 +101,32 @@ static void esp_system_event_ap_stadisconnected(void *event_handler_arg, esp_eve
              event->aid);
 }
 
-static void wifi_init_softap(struct device_wifi_config *config)
+static void wifi_init_softap(struct device_wifi_config_handle *h)
 {
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    if (h->state == WifiApStarted)
+    {
+        return;
+    }
+
+    if (h->state == WifiStaStarted)
+    {
+        esp_wifi_disconnect();
+    }
+
+    else if (h->state == WifiIdle)
+    {
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+    }
+
+    if (h->netif)
+    {
+        esp_netif_destroy_default_wifi(h->netif);
+        h->netif = NULL;
+    }
 
     uint8_t val = 0;
     tcpip_adapter_dhcps_option(TCPIP_ADAPTER_OP_SET, TCPIP_ADAPTER_ROUTER_SOLICITATION_ADDRESS, &val, sizeof(dhcps_offer_t));
-    esp_netif_create_default_wifi_ap();
+    h->netif = esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -109,17 +142,31 @@ static void wifi_init_softap(struct device_wifi_config *config)
         },
     };
 
-    memcpy(wifi_config.ap.password, config->ap.password, sizeof(wifi_config.ap.password));
+    uint8_t mac_address[8];
+    esp_err_t result;
+    result = esp_base_mac_addr_get(mac_address);
+    if (result == ESP_ERR_INVALID_MAC)
+    {
+        ESP_LOGE(__func__, "base mac address invalid.  reading from fuse.");
+        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac_address));
+        ESP_ERROR_CHECK(esp_base_mac_addr_set(mac_address));
+        ESP_LOGE(__func__, "base mac address configured.");
+    }
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+    memcpy(wifi_config.ap.password, h->config.ap.password, sizeof(wifi_config.ap.password));
     if (wifi_config.ap.password[0] == '\0')
     {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
-    memcpy(wifi_config.ap.ssid, config->ap.ssid, sizeof(wifi_config.ap.ssid));
+    memcpy(wifi_config.ap.ssid, h->config.ap.ssid, sizeof(wifi_config.ap.ssid));
     wifi_config.ap.ssid_len = strlen((const char *)wifi_config.ap.ssid);
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+
+    h->state = WifiApStarted;
 }
 
 // static esp_err_t wifi_fill_sta_config(wifi_config_t *wifi_config)
@@ -182,48 +229,83 @@ static void wifi_init_softap(struct device_wifi_config *config)
 //     }
 // }
 
-// static void wifi_init_sta(void)
-// {
-//     uint8_t mac_address[8];
-//     esp_err_t result;
+static bool wifi_init_sta(struct device_wifi_config_handle *h, struct wifi_sta_config *sta_cfg)
+{
+    wifi_config_t wifi_config;
+    uint8_t mac_address[8];
+    esp_err_t result;
 
-//     ESP_ERROR_CHECK(esp_event_loop_create_default());
-//     esp_netif_create_default_wifi_sta();
+    if (h->state == WifiStaStarted)
+    {
+        // If the Sta is the same one we're configuring, return immediately.
+        return true;
+        // ESP_ERROR_CHECK(esp_wifi_disconnect());
+    }
+    else if (h->state == WifiApStarted)
+    {
+        ESP_ERROR_CHECK(esp_wifi_disconnect());
+    }
+    else if (h->state == WifiIdle)
+    {
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+    }
 
-//     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-//     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//     wifi_config_t wifi_config;
+    if (h->netif)
+    {
+        esp_netif_destroy_default_wifi(h->netif);
+        h->netif = NULL;
+    }
 
-//     result = esp_base_mac_addr_get(mac_address);
+    h->netif = esp_netif_create_default_wifi_sta();
 
-//     if (result == ESP_ERR_INVALID_MAC)
-//     {
-//         ESP_LOGE(__func__, "base mac address invalid.  reading from fuse.");
-//         ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac_address));
-//         ESP_ERROR_CHECK(esp_base_mac_addr_set(mac_address));
-//         ESP_LOGE(__func__, "base mac address configured.");
-//     }
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-//     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-//     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-//     wifi_fill_sta_config(&wifi_config);
+    result = esp_base_mac_addr_get(mac_address);
 
-//     // ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//     // result = esp_base_mac_addr_get(mac_address);
-//     // if (result == ESP_ERR_INVALID_MAC)
-//     // {
-//     //     ESP_LOGE(__func__, "base mac address invalid.  reading from fuse.");
-//     //     ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac_address));
-//     //     ESP_ERROR_CHECK(esp_base_mac_addr_set(mac_address));
-//     //     ESP_LOGE(__func__, "base mac address configured.");
-//     // }
-//     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-//     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-//     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-//     ESP_ERROR_CHECK(esp_wifi_start());
+    if (result == ESP_ERR_INVALID_MAC)
+    {
+        ESP_LOGE(__func__, "base mac address invalid.  reading from fuse.");
+        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac_address));
+        ESP_ERROR_CHECK(esp_base_mac_addr_set(mac_address));
+        ESP_LOGE(__func__, "base mac address configured.");
+    }
 
-//     ESP_LOGI(__func__, "wifi_init_sta finished.");
-// }
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, esp_system_event_sta_start, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, esp_system_event_sta_connected, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, esp_system_event_sta_disconnected, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, esp_system_event_sta_got_ip, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_AP_STACONNECTED, esp_system_event_ap_staconnected, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_AP_STADISCONNECTED, esp_system_event_ap_stadisconnected, NULL, NULL));
+
+    strncpy((char *)wifi_config.sta.ssid, (char *)sta_cfg->ssid, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, (char *)sta_cfg->password, sizeof(wifi_config.sta.password));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    // If we're unable to connect, switch back to Idle mode.
+    result = esp_wifi_connect();
+    if (result != ESP_OK) {
+        ESP_LOGI(TAG, "Unable to connect to AP: %x (%s) -- resetting wifi", result, esp_err_to_name(result));
+        esp_netif_destroy_default_wifi(h->netif);
+        h->netif = NULL;
+        esp_event_loop_delete_default();
+        h->state = WifiIdle;
+        return false;
+    }
+
+    ESP_LOGI(__func__, "wifi_init_sta finished.");
+
+    h->state = WifiStaStarted;
+
+    return true;
+}
 
 static void fill_default_wifi_config(struct device_wifi_config *config)
 {
@@ -234,7 +316,7 @@ static void fill_default_wifi_config(struct device_wifi_config *config)
         uint8_t chip_id[8];
         esp_read_mac(chip_id, ESP_MAC_WIFI_SOFTAP);
         snprintf((char *)config->ap.ssid, sizeof(config->ap.ssid) - 1, CONFIG_ESP_WIFI_AP_SSID "-"
-                                                                                       "%02x%02x%02x%02x%02x%02x",
+                                                                                               "%02x%02x%02x%02x%02x%02x",
                  chip_id[0], chip_id[1], chip_id[2], chip_id[3], chip_id[4], chip_id[5]);
     }
     else
@@ -246,43 +328,102 @@ static void fill_default_wifi_config(struct device_wifi_config *config)
     config->magic = DEVICE_WIFI_CONFIG_MAGIC;
 }
 
+static void wifi_config_init(struct device_wifi_config_handle *h)
+{
+    size_t device_wifi_length = sizeof(h->config);
+    ESP_ERROR_CHECK(nvs_open("config", NVS_READWRITE, &h->nvs));
+
+    h->dirty = false;
+    h->state = WifiIdle;
+    h->netif = NULL;
+
+    // If the wifi key does not exist in the flash, generate a default configuration
+    if (nvs_get_blob(h->nvs, WIFI_CONFIG_NVS_KEY, &h->config, &device_wifi_length) != ESP_OK)
+    {
+        fill_default_wifi_config(&h->config);
+        h->dirty = true;
+    }
+
+    // If the wifi config is not valid (e.g. if it got damaged), regenerate it.
+    if (h->config.magic != DEVICE_WIFI_CONFIG_MAGIC)
+    {
+        fill_default_wifi_config(&h->config);
+        h->dirty = true;
+    }
+}
+
+static void wifi_config_save(struct device_wifi_config_handle *h)
+{
+    if (!h->dirty)
+    {
+        return;
+    }
+    ESP_ERROR_CHECK(nvs_set_blob(h->nvs, WIFI_CONFIG_NVS_KEY, &h->config, sizeof(h->config)));
+    h->dirty = false;
+}
+
+// Sort the stations by RSSI -- pass this function to `qsort()`.
+static int sort_by_rssi(const void *p1, const void *p2)
+{
+    const wifi_ap_record_t *left = p1;
+    const wifi_ap_record_t *right = p2;
+    return right->rssi - left->rssi;
+}
+
+static void interpret_scan_results(struct device_wifi_config_handle *h, wifi_ap_record_t *scan_results, uint16_t num_aps)
+{
+    uint16_t ap_idx;
+    uint16_t cfg_idx;
+    ESP_LOGE(TAG, "Scan results:");
+    for (ap_idx = 0; ap_idx < num_aps; ap_idx++)
+    {
+        ESP_LOGE(TAG, "RSSI: %3d  SSID: %s", scan_results[ap_idx].rssi, scan_results[ap_idx].ssid);
+        for (cfg_idx = 0; cfg_idx < 8; cfg_idx++)
+        {
+            if (!strncmp((const char *)scan_results[ap_idx].ssid, (const char *)h->config.sta[cfg_idx].ssid, sizeof(scan_results[ap_idx].ssid)))
+            {
+                if (wifi_init_sta(h, &h->config.sta[cfg_idx]))
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
+
 static void wifiTask(void *ignored)
 {
     (void)ignored;
-    nvs_handle h_nvs_conf;
-    size_t device_wifi_length = sizeof(device_wifi_config);
+    struct device_wifi_config_handle h;
 
-    bool device_wifi_config_dirty = false;
-    ESP_ERROR_CHECK(nvs_open("config", NVS_READWRITE, &h_nvs_conf));
-    if (nvs_get_blob(h_nvs_conf, WIFI_CONFIG_NVS_KEY, &device_wifi_config, &device_wifi_length) != ESP_OK)
-    {
-        fill_default_wifi_config(&device_wifi_config);
-        device_wifi_config_dirty = true;
-    }
-    if (device_wifi_config.magic != DEVICE_WIFI_CONFIG_MAGIC)
-    {
-        fill_default_wifi_config(&device_wifi_config);
-        device_wifi_config_dirty = true;
-    }
-
-    if (device_wifi_config_dirty)
-    {
-        ESP_ERROR_CHECK(nvs_set_blob(h_nvs_conf, WIFI_CONFIG_NVS_KEY, &device_wifi_config, sizeof(device_wifi_config)));
-    }
+    wifi_config_init(&h);
 
     while (1)
     {
+        // Flush the wifi config to storage if necessary.
+        wifi_config_save(&h);
 
-        // Special case -- no configured access points so begin AP mode
-        if (device_wifi_config.sta_mask == 0)
+        // Start an AP if we're in the Idle state
+        if (h.state == WifiIdle)
         {
-            wifi_init_softap(&device_wifi_config);
-            esp_wifi_set_protocol(WIFI_IF_AP, WIFI_PROTOCOL_11N);
+            wifi_init_softap(&h);
         }
-        while (1) {
 
-        taskYIELD();
-        }
+        // Scanning requires that the interface is not Idle
+        wifi_scan_config_t scan_config = {0};
+        ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, true));
+        uint16_t num_aps = 0;
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&num_aps));
+        wifi_ap_record_t scan_results[num_aps];
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&num_aps, scan_results));
+        ESP_ERROR_CHECK(esp_wifi_scan_stop());
+
+        // Sort based on SSID
+        qsort(scan_results, num_aps, sizeof(*scan_results), sort_by_rssi);
+
+        interpret_scan_results(&h, scan_results, num_aps);
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
 
